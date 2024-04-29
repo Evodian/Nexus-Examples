46c15be5750b61b0bdd97b65224cdb2c62b842a9
Aaron Pritchard <aaron.pritchard@evodian.com.au>
2024-04-29 15:01:27 +1000

    DevExpress actions

---
+++ DevExpress Example Actions/ExampleActionsWithDevExpress.cs
@@ -0,0 +1,14 @@
+﻿using DevExpress.XtraEditors;
+using System.Windows.Forms;
+
+namespace DevExpress_Example_Actions
+{
+    public class ExampleActionsWithDevExpress
+    {
+        public static bool DevExpressPopUp(string collectionIdentifier, string collectionAlternateIdentifier, string transactionType, int estimateRoute)
+        {
+            return XtraMessageBox.Show($"You have {collectionIdentifier}{(collectionAlternateIdentifier != null ? $" ({collectionAlternateIdentifier})" : "")} open.\n\nThis button was triggered from the {transactionType} tab.\n\nThe current non-stocked code estimate route open is {estimateRoute}.",
+                                    "Hello, World!", MessageBoxButtons.OKCancel) == System.Windows.Forms.DialogResult.OK;
+        }
+    }
+}
